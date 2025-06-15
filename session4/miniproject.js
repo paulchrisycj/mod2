9b81d9655b9750b66264655cf17ae08f925c9d56
Paulchris Yeow <paulchrisycj@gmail.com>
2025-06-15 15:05:37 +0800

    Mod 2 Bonus Challenge Answers :)

--- session4/miniproject.js
+++ session4/miniproject.js
@@ -4,16 +4,48 @@ const result = document.getElementById("result");
 const button = document.getElementById("calculateBtn");
  
 button.addEventListener("click", function () {
-   const bill = parseFloat(billInput.value);
-   const tipPercent = parseFloat(tipInput.value);
+  const bill = parseFloat(billInput.value);
+  const tipPercent = parseFloat(tipInput.value);
+
+  if (isNaN(bill) || isNaN(tipPercent)) {
+    result.textContent = "Please enter valid numbers.";
+    return;
+  }
+
+  const tip = bill * (tipPercent / 100);
+  const total = bill + tip;
+
+  result.textContent = `Tip: RM${tip.toFixed(2)} | Total: RM${total.toFixed(2)}`;
+});
+
+if(billInput.value == "" || tipInput.value == ""){
+    button.disabled = true
+}
  
-   if (isNaN(bill) || isNaN(tipPercent)) {
-     result.textContent = "Please enter valid numbers.";
-     return;
-   }
- 
-   const tip = bill * (tipPercent / 100);
-   const total = bill + tip;
- 
-   result.textContent = `Tip: RM${tip.toFixed(2)} | Total: RM${total.toFixed(2)}`;
- });
+ billInput.addEventListener("keyup", function(){
+  //Bonus Challenge 1
+  if(billInput.value == "" || tipInput.value == ""){
+    button.disabled = true
+  }else{
+    button.disabled = false
+    // Bonus Challenge 3
+    calculateAndShowTotal()
+  }
+})
+
+// Bonus Challenge 3
+function calculateAndShowTotal(){
+  //Actually Code inside here is the same as Line 7 to Line 18
+  const bill = parseFloat(billInput.value);
+  const tipPercent = parseFloat(tipInput.value);
+
+  // if (isNaN(bill) || isNaN(tipPercent)) {
+  //   result.textContent = "Please enter valid numbers.";
+  //   return;
+  // }
+
+  const tip = bill * (tipPercent / 100);
+  const total = bill + tip;
+
+  result.textContent = `Tip: RM${tip.toFixed(2)} | Total: RM${total.toFixed(2)}`;
+}
